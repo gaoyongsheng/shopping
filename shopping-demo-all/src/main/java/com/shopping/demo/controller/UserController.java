a5688174d31ee2d26841da8aca038467f24921e4
gaoyongsheng <gys270589787@163.com>
2020-10-22 17:59:00 +0800

    add gao

--- shopping-demo-all/src/main/java/com/shopping/demo/controller/UserController.java
+++
@@ -1,9 +0,0 @@
-package com.shopping.demo.controller;
-
-/**
- * @Author Gao
- * @Date 2020/10/22 0:31
- * @Version 1.0
- */
-public class UserController {
-}
--- shopping-demo-all/src/main/java/com/shopping/demo/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.shopping.demo.service;
-
-/**
- * @Author Gao
- * @Date 2020/10/22 0:31
- * @Version 1.0
- */
-public interface UserService {
-}
